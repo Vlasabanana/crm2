657b6e2878109a07410df73becb23b6b55ec228c
vlasabanana <vlasabanana@yandex.ru>
2021-08-17 21:18:03 +0300

    update

--- leads/views.py
+++ leads/views.py
@@ -34,20 +34,19 @@ def lead_create(request):
 
 def lead_update(request, pk):
     lead = Lead.objects.get(id=pk)
-    form = LeadForm()
+    form = LeadModelForm(instance=lead)
     if request.method == "POST":
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST, instance=lead)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            lead.first_name = first_name
-            lead.last_name = last_name
-            lead.age = age
-            lead.save()
+            form.save()
             return redirect("/leads")
     context = {
         "form":form,
         "lead": lead
     }
     return render(request, "leads/lead_update.html", context)
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")

42c4d9c3ecef2178a25044b9764d831fd9d2508e
vlasabanana <vlasabanana@yandex.ru>
2021-08-17 20:54:35 +0300

    update

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Lead, Agent
-from .forms import LeadForm
+from .forms import LeadForm, LeadModelForm
 
 def lead_list(request):
     leads = Lead.objects.all()
@@ -19,7 +19,21 @@ def lead_detail(request, pk):
     return render(request, "leads/lead_detail.html", context)
 
 
+
 def lead_create(request):
+    form = LeadModelForm()
+    if request.method == "POST":
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {
+        "form": LeadModelForm()
+    }
+    return render(request, "leads/lead_create.html", context)
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
     form = LeadForm()
     if request.method == "POST":
         form = LeadForm(request.POST)
@@ -27,15 +41,13 @@ def lead_create(request):
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
             age = form.cleaned_data['age']
-            agent = Agent.objects.first()
-            Lead.objects.create(
-                first_name=first_name,
-                last_name=last_name,
-                age=age,
-                agent=agent
-            )
+            lead.first_name = first_name
+            lead.last_name = last_name
+            lead.age = age
+            lead.save()
             return redirect("/leads")
     context = {
-        "form": LeadForm()
+        "form":form,
+        "lead": lead
     }
-    return render(request, "leads/lead_create.html", context)
+    return render(request, "leads/lead_update.html", context)
